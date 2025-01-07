cb363897320605eeef28907582f6477d56ba375f
RealXonatic <gojaheer01@gmail.com>
2025-01-07 21:04:41 +0100

    our_project

--- Project/codes/config.py
+++ Project/codes/config.py
@@ -1,15 +1,18 @@
+import torch
+
 CONFIG = {
-    "dataset_path": "/data/jahegoul10/cifar10_classes",  # Dossier contenant les classes originales
-    "split_dataset_path": "/data/jahegoul10/cifar10_split",  # Dossier pour les ensembles divisés
+    "dataset_path": "/data/jahegoul10/codes/data/cifar100_classes",  # Dossier contenant les classes originales
+    "split_dataset_path": "/data/jahegoul10/codes/data/cifar100_split",  # Dossier pour les ensembles divisés
     "output_dir": "./results",  # Dossier de sortie pour les modèles entraînés
     "minigpt4_path": "/data/jahegoul10/vicuna-7b/prerained_minigpt4_7b.pth",  # Chemin vers MiniGPT-4
     "vicuna7b_path": "/data/jahegoul10/vicuna-7b",  # Chemin vers Vicuna
     "clip_model_path": "openai/clip-vit-base-patch32",  # Modèle CLIP
     "image_size": 224,
-    "batch_size": 32,
+    "batch_size": 16,
     "epochs": 5,
     "lr": 0.001,
     "device": "cuda" if torch.cuda.is_available() else "cpu",
     "huggingface_cache": "/data/jahegoul10/huggingface_cache",
-    "num_classes": 10 # Ajoutez ceci
+    "num_classes": 10,
+    "num_tasks": 10  # Nombre total de tâches (par exemple, 10 pour CIFAR-100)
 }
